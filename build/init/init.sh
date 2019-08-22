e4368e47a45a01b94eb04122aaa06588c959a9b7
Kenny Ahrensberg <kefr@kefr.dk>
2019-08-22 21:59:36 +0000

    init.sh edited online with Bitbucket

--- build/init/init.sh
+++ build/init/init.sh
@@ -5,8 +5,8 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     echo "-- First container startup initializing --"
     dbus-uuidgen > /var/lib/dbus/machine-id
     mkdir -p /var/run/dbus
-    /usr/bin/supervisord
+    /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 else
     echo "-- Starting up normal --"
-    /usr/bin/supervisord
+    /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 fi
